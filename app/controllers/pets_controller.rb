d5d66cd3eee1d228270fb732f807f8b8d081e4a7
Laura Felce <crepu83@yahoo.it>
2019-04-10 08:54:30 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,8 +12,8 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(name: params["pet_name"])
-    if !params[:owner_name].empty? #dovrebbe essere giusto
-    @pet.owners << Owner.create(name: params["owner"]) #c'e`un errore
+    if !params[:owner][:owner_name].empty? #dovrebbe essere giusto
+    @pet.owner << Owner.create(name: params["owner"]) #c'e`un errore
     end
     redirect to "pets/#{@pet.id}"
   end
