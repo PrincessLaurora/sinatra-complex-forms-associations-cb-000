5f50855d4712644545f1c65f5799cd0831287a84
Laura Felce <crepu83@yahoo.it>
2019-04-10 09:05:43 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,7 +11,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(name: params["pet_name"])
+    @pet = Pet.create(name: params["pet"])
         binding.pry
     if !params[:owner_name].empty? #
 
