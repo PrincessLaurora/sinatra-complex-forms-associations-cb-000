9e84945092410f5db309bf34de73f7af0af133a7
Laura Felce <crepu83@yahoo.it>
2019-04-10 09:11:52 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,7 +12,7 @@ class PetsController < ApplicationController
 
   post '/pets' do
     @pet = Pet.create(name: params["pet"])
-    if !params[:owner][:name].empty? #
+    if !params["owner"]["name"].empty? #
     @pet.owner = Owner.create(name: params["owner"]["name"]) #c'e`un errore
     end
     @pet.save
