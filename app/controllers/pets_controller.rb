79ff7ffdae36172450b7178aec08c5a540428a2f
Laura Felce <crepu83@yahoo.it>
2019-04-10 10:27:08 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,9 +31,6 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    if !params[:pet].include?("owner_id")
-      params["pet"]["owner_id"] = []
-      end
     @pet = Pet.find(params[:id])
       @pet.update(params[:pet])
       if !params["owner_name"].empty?
